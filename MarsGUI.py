c6e571dee6204ed44e73d95a98d42e2ca290aa60
hamdyaea <hamdy.aea@gmail.com>
2019-03-12 21:45:58 +0100

    pictures weather

--- MarsGUI.py
+++ MarsGUI.py
@@ -42,8 +42,8 @@ def parser():
         last_date6 = mars[sol_keys_new6]["First_UTC"]
 
 
-        image = "./pictures/insight.jpg"
-        msg = ((("Mars InSight at Elysium Planitia latests weather report \n\n\nThe temperature is updated from Mars everyday\n\n\nLast sol for insight on Mars : ")+str(sol_keys_new))+str(("\n\nLast signal date : ")+str(last_date[:10]))+str(("\n\nLast signal time : ")+str(last_date[-9:-1]))+str(("\n\nMinimum Temperature : ")+str(Last_temp['mn']))+str(("\n\nMaximum Temperature : ") + str(Last_temp['mx']))+str(("\n\nAverage Temperature : ") + str(Last_temp['av'])+str(("\n\nAverage minimum temperature for the last 7 days : ")+str(((Last_temp['mn'])+(Last_temp1['mn'])+(Last_temp2['mn'])+(Last_temp3['mn'])+(Last_temp4['mn'])+(Last_temp5['mn'])+(Last_temp6['mn']))/7))+str(("\n\nAverage maximum temperature for the last 7 days : ") + str(((Last_temp['mx']) + (Last_temp1['mx']) + (Last_temp2['mx']) + (Last_temp3['mx']) + (Last_temp4['mx']) + (Last_temp5['mx']) + (Last_temp6['mx'])) / 7)))+("\n\nCurrent season on Mars : ")+str(ActuSeason))
+        image = "weather.png"
+        msg = ((("Mars InSight at Elysium Planitia latests weather report \nThe temperature is updated from Mars everyday\n\n\nLast sol for insight on Mars : ")+str(sol_keys_new))+str(("\n\nLast signal date : ")+str(last_date[:10]))+str(("\n\nLast signal time : ")+str(last_date[-9:-1]))+str(("\n\nMinimum Temperature : ")+str(Last_temp['mn']))+str(("\n\nMaximum Temperature : ") + str(Last_temp['mx']))+str(("\n\nAverage Temperature : ") + str(Last_temp['av'])+str(("\n\nAverage minimum temperature for the last 7 days : ")+str(((Last_temp['mn'])+(Last_temp1['mn'])+(Last_temp2['mn'])+(Last_temp3['mn'])+(Last_temp4['mn'])+(Last_temp5['mn'])+(Last_temp6['mn']))/7))+str(("\n\nAverage maximum temperature for the last 7 days : ") + str(((Last_temp['mx']) + (Last_temp1['mx']) + (Last_temp2['mx']) + (Last_temp3['mx']) + (Last_temp4['mx']) + (Last_temp5['mx']) + (Last_temp6['mx'])) / 7)))+("\n\nCurrent season on Mars : ")+str(ActuSeason))
         choices = ["Ok"]
         reply = buttonbox(msg, image=image, choices=choices)
 
@@ -57,6 +57,19 @@ else:
 
 url = ("https://mars.nasa.gov/rss/api/?feed=weather&category=insight&feedtype=json&ver=1.0")
 filename = wget.download(url,out="rawdata")
+
+filePath1 = "weather.png"
+
+if os.path.exists(filePath1):
+    os.remove(filePath1)
+else:
+    print("")
+
+url1 = ("https://mars.nasa.gov/rss/api/images/insight_marsweather_white.png")
+filename1 = wget.download(url1,out="weather.png")
+
+cmd = "convert -resize 25%  weather.png weather.png"
+os.system(cmd)
 
 
 parser()
