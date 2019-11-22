b89a6b85f6c09d70f3c945d1e4d70aad4816735e
hamdyaea <hamdy.aea@protonmail.com>
2019-11-22 21:05:39 +0100

    add class

--- MarsGUI.py
+++ MarsGUI.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 # Developer : Hamdy Abou El Anein
+# hamdy.aea@protonmail.com
 
 from easygui import *
 import wget
@@ -9,261 +10,358 @@ import os
 import sys
 
 
+class RoverInsight:
+    def __init__(self):
+        self.mars
+        self.sol_keys
+        self.sol_keys_new
+        self.ActuSeason
+        self.Last_temp
+        self.last_date
+        self.sol_keys_new1
+        self.Last_temp1
+        self.sol_keys_new2
+        self.Last_temp2
+        self.sol_keys_new3
+        self.Last_temp3
+        self.sol_keys_new4
+        self.Last_temp4
+        self.sol_keys_new5
+        self.Last_temp5
+        self.sol_keys_new6
+        self.Last_temp6
+        self.average_seven_min
+        self.average_seven_min_reduced
+        self.average_seven_max_reduced
+        self.average_seven_max
+        self.AverageSevenDaysReduce
+        self.AverageSevenDays
+        self.average_seven_min_reduced
+        self.average_seven_max_reduced
+        self.LastMin
+        self.LastMinReduce
+        self.LastMax
+        self.LastMaxReduce
+        self.LastAv
+        self.LastAvReduce
+        self.Last_Wnd
+        self.LastWindMin
+        self.LastWindMinReduce
+        self.LastWindMax
+        self.LastWindMaxReduce
+        self.LastWindav
+        self.LastAvReduce
+        self.Last_Wnd1
+        self.Last_Wnd2
+        self.Last_Wnd3
+        self.Last_Wnd4
+        self.Last_Wnd5
+        self.Last_Wnd6
+        self.AverageSevenDaysWind
+        self.AverageSevenDaysWindReduce
+        self.Last_Pres
+        self.LastPresMin
+        self.LastPresMinReduce
+        self.LastPresMax
+        self.LastPresMaxReduce
+        self.LastPresav
+        self.LastPresavReduce
+        self.welcome
+        self.image
+        self.msg
+
+
 def parser():
     with open("rawdata", "r") as f:
-        mars = json.load(f)
+        RoverInsight.mars = json.load(f)
 
-        sol_keys = mars["sol_keys"]
-        sol_keys_new = sol_keys[-1]
-
-        try:
-            ActuSeason = mars[sol_keys_new]["Season"]
-        except:
-            ActuSeason = "No data"
-        try:
-            Last_temp = mars[sol_keys_new]["AT"]
-        except:
-            Last_temp = 0
-        try:
-            last_date = mars[sol_keys_new]["First_UTC"]
-        except:
-            last_date = 0
+        RoverInsight.sol_keys = RoverInsight.mars["sol_keys"]
+        RoverInsight.sol_keys_new = RoverInsight.sol_keys[-1]
 
         try:
-            sol_keys_new1 = sol_keys[-2]
-            Last_temp1 = mars[sol_keys_new1]["AT"]
+            RoverInsight.ActuSeason = RoverInsight.mars[RoverInsight.sol_keys_new][
+                "Season"
+            ]
         except:
-            sol_keys_new1 = 0
-            Last_temp1 = 0
+            RoverInsight.ActuSeason = "No data"
+        try:
+            RoverInsight.Last_temp = RoverInsight.mars[RoverInsight.sol_keys_new]["AT"]
+        except:
+            RoverInsight.Last_temp = 0
+        try:
+            RoverInsight.last_date = RoverInsight.mars[RoverInsight.sol_keys_new][
+                "First_UTC"
+            ]
+        except:
+            RoverInsight.last_date = 0
 
         try:
-            sol_keys_new2 = sol_keys[-3]
-            Last_temp2 = mars[sol_keys_new2]["AT"]
+            RoverInsight.sol_keys_new1 = RoverInsight.sol_keys[-2]
+            RoverInsight.Last_temp1 = RoverInsight.mars[RoverInsight.sol_keys_new1][
+                "AT"
+            ]
         except:
-            sol_keys_new2 = 0
-            Last_temp2 = 0
+            RoverInsight.sol_keys_new1 = 0
+            RoverInsight.Last_temp1 = 0
 
         try:
-            sol_keys_new3 = sol_keys[-4]
-            Last_temp3 = mars[sol_keys_new3]["AT"]
+            RoverInsight.sol_keys_new2 = RoverInsight.sol_keys[-3]
+            RoverInsight.Last_temp2 = RoverInsight.mars[RoverInsight.sol_keys_new2][
+                "AT"
+            ]
         except:
-            sol_keys_new3 = 0
-            Last_temp3 = 0
+            RoverInsight.sol_keys_new2 = 0
+            RoverInsight.Last_temp2 = 0
 
         try:
-            sol_keys_new4 = sol_keys[-5]
-            Last_temp4 = mars[sol_keys_new4]["AT"]
+            RoverInsight.sol_keys_new3 = RoverInsight.sol_keys[-4]
+            RoverInsight.Last_temp3 = RoverInsight.mars[RoverInsight.sol_keys_new3][
+                "AT"
+            ]
         except:
-            sol_keys_new4 = 0
-            Last_temp4 = 0
+            RoverInsight.sol_keys_new3 = 0
+            RoverInsight.Last_temp3 = 0
 
         try:
-            sol_keys_new5 = sol_keys[-6]
-            Last_temp5 = mars[sol_keys_new5]["AT"]
+            RoverInsight.sol_keys_new4 = RoverInsight.sol_keys[-5]
+            RoverInsight.Last_temp4 = RoverInsight.mars[RoverInsight.sol_keys_new4][
+                "AT"
+            ]
         except:
-            sol_keys_new5 = 0
-            Last_temp5 = 0
+            RoverInsight.sol_keys_new4 = 0
+            RoverInsight.Last_temp4 = 0
 
         try:
-            sol_keys_new6 = sol_keys[-7]
-            Last_temp6 = mars[sol_keys_new6]["AT"]
+            RoverInsight.sol_keys_new5 = RoverInsight.sol_keys[-6]
+            RoverInsight.Last_temp5 = RoverInsight.mars[RoverInsight.sol_keys_new5][
+                "AT"
+            ]
         except:
-            sol_keys_new6 = 0
-            Last_temp6 = 0
+            RoverInsight.sol_keys_new5 = 0
+            RoverInsight.Last_temp5 = 0
 
         try:
-            average_seven_min = (
-                (Last_temp["mn"])
-                + (Last_temp1["mn"])
-                + (Last_temp2["mn"])
-                + (Last_temp3["mn"])
-                + (Last_temp4["mn"])
-                + (Last_temp5["mn"])
-                + (Last_temp6["mn"])
+            RoverInsight.sol_keys_new6 = RoverInsight.sol_keys[-7]
+            RoverInsight.Last_temp6 = RoverInsight.mars[RoverInsight.sol_keys_new6][
+                "AT"
+            ]
+        except:
+            RoverInsight.sol_keys_new6 = 0
+            RoverInsight.Last_temp6 = 0
+
+        try:
+            RoverInsight.average_seven_min = (
+                (RoverInsight.Last_temp["mn"])
+                + (RoverInsight.Last_temp1["mn"])
+                + (RoverInsight.Last_temp2["mn"])
+                + (RoverInsight.Last_temp3["mn"])
+                + (RoverInsight.Last_temp4["mn"])
+                + (RoverInsight.Last_temp5["mn"])
+                + (RoverInsight.Last_temp6["mn"])
             ) / 7
-            average_seven_min_reduced = round(average_seven_min, 2)
+            RoverInsight.average_seven_min_reduced = round(
+                RoverInsight.average_seven_min, 2
+            )
 
-            average_seven_max = (
-                (Last_temp["mx"])
-                + (Last_temp1["mx"])
-                + (Last_temp2["mx"])
-                + (Last_temp3["mx"])
-                + (Last_temp4["mx"])
-                + (Last_temp5["mx"])
-                + (Last_temp6["mx"])
+            RoverInsight.average_seven_max = (
+                (RoverInsight.Last_temp["mx"])
+                + (RoverInsight.Last_temp1["mx"])
+                + (RoverInsight.Last_temp2["mx"])
+                + (RoverInsight.Last_temp3["mx"])
+                + (RoverInsight.Last_temp4["mx"])
+                + (RoverInsight.Last_temp5["mx"])
+                + (RoverInsight.Last_temp6["mx"])
             ) / 7
-            average_seven_max_reduced = round(average_seven_max, 2)
+            RoverInsight.average_seven_max_reduced = round(
+                RoverInsight.average_seven_max, 2
+            )
 
-            AverageSevenDays = (
-                Last_temp["av"]
-                + Last_temp1["av"]
-                + Last_temp2["av"]
-                + Last_temp3["av"]
-                + Last_temp4["av"]
-                + Last_temp5["av"]
-                + Last_temp6["av"]
+            RoverInsight.AverageSevenDays = (
+                RoverInsight.Last_temp["av"]
+                + RoverInsight.Last_temp1["av"]
+                + RoverInsight.Last_temp2["av"]
+                + RoverInsight.Last_temp3["av"]
+                + RoverInsight.Last_temp4["av"]
+                + RoverInsight.Last_temp5["av"]
+                + RoverInsight.Last_temp6["av"]
             ) / 7
-            AverageSevenDaysReduce = round(AverageSevenDays, 2)
+            RoverInsight.AverageSevenDaysReduce = round(
+                RoverInsight.AverageSevenDays, 2
+            )
         except:
-            average_seven_min_reduced = 0
-            average_seven_max_reduced = 0
-            AverageSevenDaysReduce = 0
+            RoverInsight.average_seven_min_reduced = 0
+            RoverInsight.average_seven_max_reduced = 0
+            RoverInsight.AverageSevenDaysReduce = 0
 
         try:
-            LastMin = Last_temp["mn"]
-            LastMinReduce = round(LastMin, 2)
+            RoverInsight.LastMin = RoverInsight.Last_temp["mn"]
+            RoverInsight.LastMinReduce = round(RoverInsight.LastMin, 2)
 
-            LastMax = Last_temp["mx"]
-            LastMaxReduce = round(LastMax, 2)
+            RoverInsight.LastMax = RoverInsight.Last_temp["mx"]
+            RoverInsight.LastMaxReduce = round(RoverInsight.LastMax, 2)
 
-            LastAv = Last_temp["av"]
-            LastAvReduce = round(LastAv, 2)
+            RoverInsight.LastAv = RoverInsight.Last_temp["av"]
+            RoverInsight.LastAvReduce = round(RoverInsight.LastAv, 2)
         except:
-            LastMinReduce = 0
-            LastMaxReduce = 0
-            LastAvReduce = 0
+            RoverInsight.LastMinReduce = 0
+            RoverInsight.LastMaxReduce = 0
+            RoverInsight.LastAvReduce = 0
 
         # Wind speed
 
         try:
-            Last_Wnd = mars[sol_keys_new]["HWS"]
+            RoverInsight.Last_Wnd = RoverInsight.mars[RoverInsight.sol_keys_new]["HWS"]
 
-            LastWindMin = Last_Wnd["mn"] * 3.6
-            LastWindMinReduce = round(LastWindMin, 2)
+            RoverInsight.LastWindMin = RoverInsight.Last_Wnd["mn"] * 3.6
+            RoverInsight.LastWindMinReduce = round(RoverInsight.LastWindMin, 2)
 
-            LastWindMax = Last_Wnd["mx"] * 3.6
-            LastWindMaxReduce = round(LastWindMax, 2)
+            RoverInsight.LastWindMax = RoverInsight.Last_Wnd["mx"] * 3.6
+            RoverInsight.LastWindMaxReduce = round(RoverInsight.LastWindMax, 2)
 
-            LastWindav = Last_Wnd["av"] * 3.6
-            LastWindavReduce = round(LastWindav, 2)
+            RoverInsight.LastWindav = RoverInsight.Last_Wnd["av"] * 3.6
+            RoverInsight.LastWindavReduce = round(RoverInsight.LastWindav, 2)
         except:
-            LastWindMinReduce = 0
-            LastWindMaxReduce = 0
-            LastWindavReduce = 0
-
-        try:
-            Last_Wnd1 = mars[sol_keys_new1]["HWS"]
-        except:
-            Last_Wnd1 = 0
-        try:
-            Last_Wnd2 = mars[sol_keys_new2]["HWS"]
-        except:
-            Last_Wnd2 = 0
-        try:
-            Last_Wnd3 = mars[sol_keys_new3]["HWS"]
-        except:
-            Last_Wnd3 = 0
-        try:
-            Last_Wnd4 = mars[sol_keys_new4]["HWS"]
-        except:
-            Last_Wnd4 = 0
-        try:
-            Last_Wnd5 = mars[sol_keys_new5]["HWS"]
-        except:
-            Last_Wnd5 = 0
-        try:
-            Last_Wnd6 = mars[sol_keys_new6]["HWS"]
-        except:
-            Last_Wnd6 = 0
+            RoverInsight.LastWindMinReduce = 0
+            RoverInsight.LastWindMaxReduce = 0
+            RoverInsight.LastWindavReduce = 0
 
         try:
-            AverageSevenDaysWind = (
-                Last_Wnd["av"]
-                + Last_Wnd1["av"]
-                + Last_Wnd2["av"]
-                + Last_Wnd3["av"]
-                + Last_Wnd4["av"]
-                + Last_Wnd5["av"]
-                + Last_Wnd6["av"]
+            RoverInsight.Last_Wnd1 = RoverInsight.mars[RoverInsight.sol_keys_new1][
+                "HWS"
+            ]
+        except:
+            RoverInsight.Last_Wnd1 = 0
+        try:
+            RoverInsight.Last_Wnd2 = RoverInsight.mars[RoverInsight.sol_keys_new2][
+                "HWS"
+            ]
+        except:
+            RoverInsight.Last_Wnd2 = 0
+        try:
+            RoverInsight.Last_Wnd3 = RoverInsight.mars[RoverInsight.sol_keys_new3][
+                "HWS"
+            ]
+        except:
+            RoverInsight.Last_Wnd3 = 0
+        try:
+            RoverInsight.Last_Wnd4 = RoverInsight.mars[RoverInsight.sol_keys_new4][
+                "HWS"
+            ]
+        except:
+            RoverInsight.Last_Wnd4 = 0
+        try:
+            RoverInsight.Last_Wnd5 = RoverInsight.mars[RoverInsight.sol_keys_new5][
+                "HWS"
+            ]
+        except:
+            RoverInsight.Last_Wnd5 = 0
+        try:
+            RoverInsight.Last_Wnd6 = RoverInsight.mars[RoverInsight.sol_keys_new6][
+                "HWS"
+            ]
+        except:
+            RoverInsight.Last_Wnd6 = 0
+
+        try:
+            RoverInsight.AverageSevenDaysWind = (
+                RoverInsight.Last_Wnd["av"]
+                + RoverInsight.Last_Wnd1["av"]
+                + RoverInsight.Last_Wnd2["av"]
+                + RoverInsight.Last_Wnd3["av"]
+                + RoverInsight.Last_Wnd4["av"]
+                + RoverInsight.Last_Wnd5["av"]
+                + RoverInsight.Last_Wnd6["av"]
             ) / 7
-            AverageSevenDaysWindReduce = round(AverageSevenDaysWind, 2)
+            RoverInsight.AverageSevenDaysWindReduce = round(
+                RoverInsight.AverageSevenDaysWind, 2
+            )
         except:
-            AverageSevenDaysWindReduce = 0
+            RoverInsight.AverageSevenDaysWindReduce = 0
 
         # Pressure Atm
         try:
-            Last_Pres = mars[sol_keys_new]["PRE"]
+            RoverInsight.Last_Pres = RoverInsight.mars[RoverInsight.sol_keys_new]["PRE"]
 
-            LastPresMin = Last_Pres["mn"]
-            LastPresMinReduce = round(LastPresMin, 2)
+            RoverInsight.LastPresMin = RoverInsight.Last_Pres["mn"]
+            RoverInsight.LastPresMinReduce = round(RoverInsight.LastPresMin, 2)
 
-            LastPresMax = Last_Pres["mx"]
-            LastPresMaxReduce = round(LastPresMax, 2)
+            RoverInsight.LastPresMax = RoverInsight.Last_Pres["mx"]
+            RoverInsight.LastPresMaxReduce = round(RoverInsight.LastPresMax, 2)
 
-            LastPresav = Last_Pres["av"]
-            LastPresavReduce = round(LastPresav, 2)
+            RoverInsight.LastPresav = RoverInsight.Last_Pres["av"]
+            RoverInsight.LastPresavReduce = round(RoverInsight.LastPresav, 2)
         except:
-            LastPresMinReduce = 0
-            LastPresMaxReduce = 0
-            LastPresavReduce = 0
+            RoverInsight.LastPresMinReduce = 0
+            RoverInsight.LastPresMaxReduce = 0
+            RoverInsight.LastPresavReduce = 0
 
         # GUI creation
 
-        welcome = "Mars InSight at Elysium Planitia latests weather report"
+        RoverInsight.welcome = "Mars InSight at Elysium Planitia latests weather report"
 
-        image = ("weatherOK.png", "insightLab.jpg")
-        msg = (
-            (welcome)
+        RoverInsight.image = ("weatherOK.png", "insightLab.jpg")
+        RoverInsight.msg = (
+            (RoverInsight.welcome)
             + str("\nThe temperature is updated from Mars everyday")
             + str("\n\nLast sol for insight on Mars : ")
-            + str(sol_keys_new)
+            + str(RoverInsight.sol_keys_new)
             + str("          ")
             + str("Minimum Wind Speed ")
-            + str(LastWindMinReduce)
+            + str(RoverInsight.LastWindMinReduce)
             + str(" km/h")
             + str("\n\nLast signal date : ")
-            + str(last_date[:10])
+            + str(RoverInsight.last_date[:10])
             + str("               ")
             + str("Maximum Wind Speed ")
-            + str(LastWindMaxReduce)
+            + str(RoverInsight.LastWindMaxReduce)
             + str(" km/h")
             + str("\n\nLast signal time : ")
-            + str(last_date[-9:-1])
+            + str(RoverInsight.last_date[-9:-1])
             + str(" UTC")
             + str("             ")
             + str("Average Wind Speed ")
-            + str(LastWindavReduce)
+            + str(RoverInsight.LastWindavReduce)
             + str(" km/h")
             + str("\n\nMinimum Temperature : ")
-            + str(LastMinReduce)
+            + str(RoverInsight.LastMinReduce)
             + str(" °C")
             + str("             ")
             + str("Average 7 days Wind Speed ")
-            + str(AverageSevenDaysWindReduce)
+            + str(RoverInsight.AverageSevenDaysWindReduce)
             + str(" km/h")
             + str("\n\nMaximum Temperature : ")
-            + str(LastMaxReduce)
+            + str(RoverInsight.LastMaxReduce)
             + str(" °C")
             + str("             ")
             + str("Minimum Air Pressure ")
-            + str(LastPresMinReduce)
+            + str(RoverInsight.LastPresMinReduce)
             + str(" Pa")
             + str("\n\nAverage Temperature : ")
-            + str(LastAvReduce)
+            + str(RoverInsight.LastAvReduce)
             + str(" °C")
             + str("             ")
             + str("Maximum Air Pressure ")
-            + str(LastPresMaxReduce)
+            + str(RoverInsight.LastPresMaxReduce)
             + str(" Pa")
             + str("\n\nAverage 7 days temperature : ")
-            + str(AverageSevenDaysReduce)
+            + str(RoverInsight.AverageSevenDaysReduce)
             + str(" °C")
             + str("      ")
             + str("     Average Air Pressure ")
-            + str(LastPresavReduce)
+            + str(RoverInsight.LastPresavReduce)
             + str(" Pa")
             + str("\n\nAverage 7 days minimum temperature : ")
-            + str(average_seven_min_reduced)
+            + str(RoverInsight.average_seven_min_reduced)
             + str(" °C")
             + str("\n\nAverage 7 days maximum temperature : ")
-            + str(average_seven_max_reduced)
+            + str(RoverInsight.average_seven_max_reduced)
             + str(" °C")
             + ("\n\nCurrent season on Mars : ")
-            + str((ActuSeason).title())
+            + str((RoverInsight.ActuSeason).title())
         )
 
         choices = ["Ok"]
-        reply = buttonbox(msg, image=image, choices=choices)
+        reply = buttonbox(RoverInsight.msg, image=RoverInsight.image, choices=choices)
         if reply == "Ok":
             sys.exit(0)
         elif reply == "weatherOK.png":
